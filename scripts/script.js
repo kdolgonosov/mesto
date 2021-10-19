35744505bc4971dc556c26c4c2b3f2b017e894ec
unknown <dolgonosov.konstantin@yandex.ru>
2021-10-19 17:08:03 +0300

    3rd try

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,9 @@
-let popUp = document.querySelector('.pop-up__overlay')
+let popUp = document.querySelector('.pop-up')
 let showPopUpBtn = document.querySelector('.profile__edit-button')
 let hidePopUpBtn = document.querySelector('.pop-up__close-btn')
 let saveInfoBtn = document.querySelector('.pop-up__submit-btn')
-let inputName = document.querySelector('.pop-up__input_name')
-let inputProfession = document.querySelector('.pop-up__input_profession')
+let inputName = document.querySelector('.pop-up__input_type_name')
+let inputProfession = document.querySelector('.pop-up__input_type_profession')
 
 inputName.value = 'Жак-Ив Кусто'
 inputProfession.value = 'Исследователь океана'
@@ -15,14 +15,14 @@ function showPopUp () {
 
 function hidePopUp () {
   popUp.classList.add('pop-up_hidden')
-  document.querySelector('.pop-up__input_name').value = document.querySelector('.profile__info-item-name').textContent
-  document.querySelector('.pop-up__input_profession').value = document.querySelector('.profile__info-item-profession').textContent
+  document.querySelector('.pop-up__input_type_name').value = document.querySelector('.profile__info-item-name').textContent
+  document.querySelector('.pop-up__input_type_profession').value = document.querySelector('.profile__info-item-profession').textContent
   document.querySelector('.main').classList.remove('main__scroll-lock')
 }
 
 function editInfo() {
-  let inputName = document.querySelector('.pop-up__input_name')
-  let inputProfession = document.querySelector('.pop-up__input_profession')
+  let inputName = document.querySelector('.pop-up__input_type_name')
+  let inputProfession = document.querySelector('.pop-up__input_type_profession')
   document.querySelector('.profile__info-item-name').textContent = `${inputName.value}`  
   document.querySelector('.profile__info-item-profession').textContent = `${inputProfession.value}`
   hidePopUp()
