9795f547be059cc6895399669df5a8029ffd7d64
unknown <dolgonosov.konstantin@yandex.ru>
2021-10-21 14:10:54 +0300

    fixes after 2nd review

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,12 @@
 let popUp = document.querySelector('.pop-up')
 let showPopUpBtn = document.querySelector('.profile__edit-button')
 let hidePopUpBtn = document.querySelector('.pop-up__close-btn')
-let saveInfoBtn = document.querySelector('.pop-up__submit-btn')
 let inputName = document.querySelector('.pop-up__input_type_name')
 let inputProfession = document.querySelector('.pop-up__input_type_profession')
 let itemName = document.querySelector('.profile__info-item-name')
 let itemProfession = document.querySelector('.profile__info-item-profession')
+
+let editForm = document.querySelector('.pop-up__form')
 
 function showPopUp () {
   inputName.value = itemName.textContent
@@ -20,6 +21,7 @@ function hidePopUp () {
 }
 
 function editInfo() {
+  event.preventDefault()
   itemName.textContent = `${inputName.value}`  
   itemProfession.textContent = `${inputProfession.value}`
   hidePopUp()
@@ -27,3 +29,4 @@ function editInfo() {
 
 showPopUpBtn.addEventListener('click', showPopUp);
 hidePopUpBtn.addEventListener('click', hidePopUp);
+editForm.addEventListener('submit', editInfo);
